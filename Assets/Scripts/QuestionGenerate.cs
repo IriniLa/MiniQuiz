a1a8c88db1aab2de6b5909b14c7b436bf5005906
IriniLa <112558707+IriniLa@users.noreply.github.com>
2023-09-14 17:38:51 +0300

    V2
    version 2

--- Assets/Scripts/QuestionGenerate.cs
+++ Assets/Scripts/QuestionGenerate.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +7,59 @@ public class QuestionGenerate : MonoBehaviour
     public static string actualAnswer;
     public static bool displayingQuestion = false;
 
+    public int questionNumber;
+
 
     void Update()
     {
         if (displayingQuestion == false)
-        {
+        {   
             displayingQuestion = true;
-            QuestionDisplay.newQuestion = " How many planets do we have?";
-            QuestionDisplay.newA = "A. 12 planets";
-            QuestionDisplay.newB = "B. 2 planets";
-            QuestionDisplay.newC = "C. 100 planets";
-            QuestionDisplay.newD = "D. 8 planets";
-            actualAnswer = "D";
+            questionNumber = Random.Range(1, 5);
+
+            if (questionNumber == 1)
+            {
+                
+                QuestionDisplay.newQuestion = "Ποια απο τις παρακάτω εντολές τυπώνει το \"Hello World!\" στην οθόνη;";
+                QuestionDisplay.newA = "Α. print(\"Hello World\");";
+                QuestionDisplay.newB = "Β. printf(\"Hello World!\");";
+                QuestionDisplay.newC = "Γ. printf(\"Hello World!\")";
+                QuestionDisplay.newD = "Δ. printf(Hello World!);";
+                actualAnswer = "Β";
+            }
+
+            if (questionNumber == 2)
+            {
+                QuestionDisplay.newQuestion = "Ποιον τύπο δεδομένων χρησιμοποιούμε για την δήλωση ακεραίων μεταβλητών;";
+                QuestionDisplay.newA = "Α. char";
+                QuestionDisplay.newB = "Β. float";
+                QuestionDisplay.newC = "Γ. int";
+                QuestionDisplay.newD = "Δ. double";
+                actualAnswer = "Γ";
+            }
+
+            if (questionNumber == 3)
+            {
+                QuestionDisplay.newQuestion = "Ποιον τύπο δεδομένων χρησιμοποιούμε για την δήλωση πραγματικών μεταβλητων;";
+                QuestionDisplay.newA = "Α. char";
+                QuestionDisplay.newB = "Β. float";
+                QuestionDisplay.newC = "Γ. int";
+                QuestionDisplay.newD = "Δ. κανένα απο τα παραπάνω";
+                actualAnswer = "Β";
+            }
+
+            if (questionNumber == 4)
+            {
+                QuestionDisplay.newQuestion = "Ποια ακολουθία διαφυγής χρησιμοποιούμε για να αλλάξουμε γραμμή;";
+                QuestionDisplay.newA = "Α. \\n";
+                QuestionDisplay.newB = "Β. \\b";
+                QuestionDisplay.newC = "Γ. \\t";
+                QuestionDisplay.newD = "Δ. \\\"";
+                actualAnswer = "Α";
+            }
+
+            // all question go above this line
+            QuestionDisplay.pleaseUpdate = false;
         }
     }
 }
